634fdb7c16a758beb1040fcd9ab3f2e15fd85982
Chris Arnault <chris.arnault.1@gmail.com>
2024-03-28 23:52:41 +0100

    Finalisation de la procédure d'installation build.bat pour la construction du kit puis installation dans le Store de test install.bat pour l'installation dans le Store public

--- build.bat
+++ build.bat
@@ -8,4 +8,3 @@ tar tzf dist\%PACKAGE%-%VERSION%.tar.gz
 set PASS=pypi-AgENdGVzdC5weXBpLm9yZwIkNzk4NjU0OWItNzFhNC00NGRjLTliY2ItNWNhNDRjOGQxM2VmAAIqWzMsIjI0NzM1Nzk0LTBiYjItNDFjNS1hNzE0LWI2ZDExMzQ5MDk0OSJdAAAGIOFTJkKGdYAQN519yfHKIxRdP9BU-9QhqZoj9zoYmq6t
 twine upload -u __token__ -p %PASS% --repository-url https://test.pypi.org/legacy/ dist\%PACKAGE%-%VERSION%*.*
 REM pip install -i https://test.pypi.org/simple/ AnumbyMasterMind==%VERSION%
-REM twine upload -u __token__ -p %PASS% dist\%PACKAGE%-%VERSION%*.*
--- install.bat
+++ install.bat
@@ -1,11 +1,5 @@
 set /P VERSION=<VERSION
 echo "version = %VERSION%"
 set PACKAGE=AnumbyMasterMind
-REM rmdir dist /s/q
-REM python setup.py sdist bdist_wheel
-REM twine check dist/%PACKAGE%-%VERSION%-py3-none-any.whl
-REM tar tzf dist\%PACKAGE%-%VERSION%.tar.gz
-set PASS=pypi-AgENdGVzdC5weXBpLm9yZwIkNzk4NjU0OWItNzFhNC00NGRjLTliY2ItNWNhNDRjOGQxM2VmAAIqWzMsIjI0NzM1Nzk0LTBiYjItNDFjNS1hNzE0LWI2ZDExMzQ5MDk0OSJdAAAGIOFTJkKGdYAQN519yfHKIxRdP9BU-9QhqZoj9zoYmq6t
-REM twine upload -u __token__ -p %PASS% --repository-url https://test.pypi.org/legacy/ dist\%PACKAGE%-%VERSION%*.*
-REM pip install -i https://test.pypi.org/simple/ AnumbyMasterMind==%VERSION%
+set PASS=pypi-AgEIcHlwaS5vcmcCJDQ4YWNjNzIwLTIyYTgtNDAwYi1hNDY2LTQ0NjhmZGNkNTBiNQACKlszLCIyMDhmMTYxMi1lMTY1LTRiOWUtOTY2Zi1lNDY4YjIwYjIyODEiXQAABiBtv16xjrxHSd6Wxlg-xIe5qgntUuFCk-ViEIlbpilqDg
 twine upload -u __token__ -p %PASS% dist\%PACKAGE%-%VERSION%*.*
