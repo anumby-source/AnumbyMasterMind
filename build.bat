653fc3385e43d5523aa24fb5e1c8167e449e3653
Chris Arnault <chris.arnault.1@gmail.com>
2024-03-28 21:42:56 +0100

    Première implémentation du package AnumbyMasterMind Le code hérite du package développé dans https://github.com/anumby-source/RobotServiceJeunesse2024 Et est installé dans PyPI

---
+++ build.bat
@@ -0,0 +1,10 @@
+set /P VERSION=<VERSION
+echo "version = %VERSION%"
+set PACKAGE=AnumbyMasterMind
+REM rmdir dist /s/q
+python setup.py sdist bdist_wheel
+twine check dist/%PACKAGE%-%VERSION%-py3-none-any.whl
+tar tzf dist\%PACKAGE%-%VERSION%.tar.gz
+twine upload -u chris.arnault --repository-url https://test.pypi.org/legacy/ dist\%PACKAGE%-%VERSION%*.*
+REM pip install -i https://test.pypi.org/simple/ AnumbyMasterMind==%VERSION%
+REM twine upload -u chris.arnault dist\%PACKAGE%-%VERSION%*.*
